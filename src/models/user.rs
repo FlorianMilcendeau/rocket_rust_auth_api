3bbd0b0e0888b372917436147931c07cb5ad967f
Florian Milcendeau <fmilcendeau@listopaye.fr>
2022-01-01 17:50:56 +0100

    Update: user model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,3 +1,6 @@
+use rocket::serde::{Deserialize, Serialize};
+use crate::schema::user;
+
 #[derive(Queryable, AsChangeset, Serialize, Deserialize, Debug)]
 #[table_name = "user"]
 pub struct User {
@@ -6,9 +9,9 @@ pub struct User {
     password: String,
 }
 
-#[derive(Insertable,  Serialize, Deserialize)]
+#[derive(Insertable, Serialize, Deserialize)]
 #[table_name = "user"]
 pub struct NewUser {
-    email: String,
-    password: String,
+    pub email: String,
+    pub password: String,
 }
