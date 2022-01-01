3fc853b41d938c7338e6bd81d29a7061fe684ec6
Florian Milcendeau <fmilcendeau@listopaye.fr>
2022-01-01 17:53:12 +0100

    Addition: user signup

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ extern crate rocket;
 extern crate diesel;
 extern crate dotenv;
 
-
 use dotenv::dotenv;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 
 mod connection;
-use connection::init_pool;
-
+use connection::{init_pool, DbConn};
+mod repository;
+use repository::user_repository::create_user;
+mod models;
+use models::user::{NewUser, User as DbUser};
 mod utils;
 use utils::password::generate_password;
+mod schema;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -26,12 +29,15 @@ struct User<'r> {
 }
 
 #[post("/signup", data = "<user>")]
-fn signin(user: Json<User<'_>>) -> Json<User<'_>> {
+fn signup(user: Json<User<'_>>, connection: DbConn) -> Json<DbUser> {
     let password = generate_password(user.password.clone());
-    Json(User {
-        email: user.email,
-        password: password,
-    })
+    let new_user = NewUser {
+        email: user.email.to_string(),
+        password,
+    };
+    let user_created = create_user(new_user, &connection).unwrap();
+
+    Json(user_created)
 }
 
 #[launch]
@@ -39,5 +45,5 @@ fn rocket() -> _ {
     dotenv().ok();
     rocket::build()
         .manage(init_pool())
-        .mount("/api", routes![index, signin])
+        .mount("/api", routes![index, signup])
 }
