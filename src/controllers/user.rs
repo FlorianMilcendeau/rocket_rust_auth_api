8e5130f2c8d9120f2166f06a9d6c7a52b9e95949
Florian Milcendeau <fmilcendeau@listopaye.fr>
2022-01-02 16:47:23 +0100

    Addition: create user controller

---
+++ src/controllers/user.rs
@@ -0,0 +1,24 @@
+use rocket::serde::json::Json;
+use rocket::http::Status;
+use diesel::result::Error;
+
+use crate::connection::DbConn;
+use crate::models::user::User as DbUser;
+use crate::repository::user_repository::get_user_by_id;
+
+/// Place is temporary.
+fn error_status(error: Error) -> Status {
+    match error {
+        Error::NotFound => Status::NotFound,
+        _ => Status::InternalServerError,
+    }
+}
+
+#[get("/<id>")]
+pub fn get_user(id: i32, connection: DbConn) -> Result<Json<DbUser>, Status> {
+    let user = get_user_by_id(id, &connection)
+        .map(|user| Json(user))
+        .map_err(|error| error_status(error));
+
+    user
+}
