949b1c1e9064ee76aab3f034fb0275fcb13ef74b
Florian Milcendeau <fmilcendeau85@gmail.com>
2022-01-09 15:29:33 +0100

    add: decode jwt

--- src/utils/jsonwebtoken.rs
+++ src/utils/jsonwebtoken.rs
@@ -1,12 +1,20 @@
-use jsonwebtoken::{encode, errors::Error, Algorithm, EncodingKey, Header};
-use serde::{Deserialize, Serialize};
+use chrono::prelude::*;
+use jsonwebtoken::{
+    decode, encode, errors::Error, Algorithm, DecodingKey, EncodingKey, Header, Validation,
+};
+use regex::Regex;
+use rocket::http::Status;
+use rocket::serde::{json::json, Deserialize, Serialize};
+
+use crate::models::user::User;
+use crate::utils::http::ApiResponse;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: i32,
     pub name: String,
     pub email: String,
-    exp: usize,
+    exp: i64,
 }
 
 impl Claims {
@@ -15,7 +23,7 @@ impl Claims {
             sub,
             name: name.to_owned(),
             email: email.to_owned(),
-            exp: 1000000,
+            exp: Utc::now().timestamp() + 1000,
         }
     }
 }
@@ -28,4 +36,36 @@ pub fn generate_json_web_token(claims: Claims) -> Result<String, Error> {
     );
 
     return token;
+}
+
+pub fn decode_json_web_token(token: &str) -> Result<User, Error> {
+    let claims = decode::<Claims>(
+        token,
+        &DecodingKey::from_rsa_pem(include_bytes!("../../id_rsa_pub.pem"))?,
+        &Validation::new(Algorithm::RS256),
+    );
+
+    match claims {
+        Ok(value) => Ok(User {
+            id: value.claims.sub,
+            name: value.claims.name,
+            email: value.claims.email,
+            password: "".to_string(),
+        }),
+        Err(error) => Err(error),
+    }
+}
+
+pub fn extract_bearer_token(token: &str) -> Result<String, ApiResponse> {
+    let reg = Regex::new(r"(Bearer)\s+(\S+)").unwrap();
+    let captures = reg.captures(token).unwrap();
+
+    if captures.len() == 3 {
+        return Ok(String::from(&captures[2]));
+    }
+
+    return Err(ApiResponse::new(
+        Status::Unauthorized,
+        json!("You must provided Bearer token"),
+    ));
 }
